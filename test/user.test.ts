5914cd003bdfbaeb1164c434a0677b3e240db4de
ilhambonardoo <ilhambonardo123@gmail.com>
2025-12-28 22:00:20 +0700

    unitesting GET USER API

--- test/user.test.ts
+++ test/user.test.ts
@@ -128,3 +128,34 @@ describe("POST /api/users/login", () => {
     expect(response.body.errors).toBeDefined();
   });
 });
+
+describe("GET /api/users/current", () => {
+  beforeEach(async () => {
+    await UserTest.create();
+  });
+
+  afterEach(async () => {
+    await UserTest.delete();
+  });
+
+  it("should be able to get user", async () => {
+    const response = await supertest(web)
+      .get("/api/users/current")
+      .set("X-API-TOKEN", "test");
+
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.username).toBe("test");
+    expect(response.body.data.name).toBe("test");
+  });
+
+  it("should be reject get user if token is invalide", async () => {
+    const response = await supertest(web)
+      .get("/api/users/current")
+      .set("X-API-TOKEN", "salah");
+
+    logger.debug(response.body);
+    expect(response.status).toBe(401);
+    expect(response.body.errors).toBeDefined();
+  });
+});
