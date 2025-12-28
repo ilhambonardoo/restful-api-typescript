9887052d3f514d2fb9495324c8890a21c208ce52
ilhambonardoo <ilhambonardo123@gmail.com>
2025-12-28 15:22:32 +0700

    menambahkan endpoint register dan routing publik untuk login --> /api/users/login

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,5 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
-import type { CreateUserRequest } from "../model/user-model";
+import type { CreateUserRequest, LoginUserRequest } from "../model/user-model";
 import { UserService } from "../service/user-service";
 
 export class UserController {
@@ -7,6 +7,17 @@ export class UserController {
     try {
       const request: CreateUserRequest = req.body as CreateUserRequest;
       const response = await UserService.register(request);
+      res.status(200).json({
+        data: response,
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+  static async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const request: LoginUserRequest = req.body as LoginUserRequest;
+      const response = await UserService.login(request);
       res.status(200).json({
         data: response,
       });
