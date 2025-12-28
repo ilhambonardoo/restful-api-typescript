363092d415764f16dc7ad705acc24bc826ffaa34
ilhambonardoo <ilhambonardo123@gmail.com>
2025-12-28 21:59:25 +0700

    buat endpoint dan buat api untuk mendapatkan login user

---
+++ src/route/api.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { authMiddleware } from "../middleware/auth-middleware";
+import { UserController } from "../controller/user-controller";
+
+export const apiRouter = express();
+apiRouter.use(authMiddleware);
+
+apiRouter.get("/api/users/current", UserController.get);
