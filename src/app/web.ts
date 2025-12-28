599b46a6a728e4b9fb1a2ba4f0d810b397aa5261
ilhambonardoo <ilhambonardo123@gmail.com>
2025-12-28 21:59:55 +0700

    api router yang sudah dibuat dipakai di web

--- src/app/web.ts
+++ src/app/web.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { publicRouter } from "../route/public-api";
 import { erorrMiddleware } from "../middleware/error-middleware";
+import { apiRouter } from "../route/api";
 
 export const web = express();
 
 web.use(express.json());
 web.use(publicRouter);
+web.use(apiRouter);
 web.use(erorrMiddleware);
