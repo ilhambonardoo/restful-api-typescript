f00ae161ca3897af318a7e26a605358fab271991
ilhambonardoo <ilhambonardo123@gmail.com>
2026-01-01 16:23:30 +0700

    buat validation untuk update contact dan bussniess logic untuk update contact

--- src/service/contact-service.ts
+++ src/service/contact-service.ts
@@ -1,10 +1,11 @@
 import { prismaClient } from "../app/database";
 import { ResponseError } from "../error/response-error";
-import { type User } from "../generated/prisma/client";
+import { type Contact, type User } from "../generated/prisma/client";
 import {
   toContactResponse,
   type ContactResponse,
   type CreateContactRequest,
+  type UpdateContactRequest,
 } from "../model/contact-model";
 import { ContactValidaton } from "../validation/contact-validation";
 import { Validation } from "../validation/validation";
@@ -27,17 +28,43 @@ export class ContactService {
     return toContactResponse(contact);
   }
 
-  static async get(user: User, id: number): Promise<ContactResponse> {
+  static async checkContactMustExists(
+    username: string,
+    id: number
+  ): Promise<Contact> {
     const contact = await prismaClient.contact.findUnique({
       where: {
         id: id,
-        username: user.username,
+        username: username,
       },
     });
 
     if (!contact) {
       throw new ResponseError(404, "Contact not found");
     }
+
+    return contact;
+  }
+
+  static async get(user: User, id: number): Promise<ContactResponse> {
+    const contact = await this.checkContactMustExists(user.username, id);
+    return toContactResponse(contact);
+  }
+
+  static async update(
+    user: User,
+    request: UpdateContactRequest
+  ): Promise<ContactResponse> {
+    const updateRequest = Validation.validate(ContactValidaton.UPDATE, request);
+    await this.checkContactMustExists(user.username, updateRequest.id);
+
+    const contact = await prismaClient.contact.update({
+      where: {
+        id: updateRequest.id,
+        username: user.username,
+      },
+      data: updateRequest,
+    });
 
     return toContactResponse(contact);
   }
