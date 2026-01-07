66d2ab190272faf4ddb4b31fbc914ae2378882ec
ilhambonardoo <ilhambonardo123@gmail.com>
2026-01-07 22:46:09 +0700

    membuat bussniess logic get address request

--- src/service/address-service.ts
+++ src/service/address-service.ts
@@ -1,9 +1,11 @@
 import { prismaClient } from "../app/database";
+import { ResponseError } from "../error/response-error";
 import type { User } from "../generated/prisma/client";
 import {
   toAddressResponse,
   type AddressResponse,
   type CreateAddressRequest,
+  type GetAddressRequest,
 } from "../model/address-model";
 import { AddressValidation } from "../validation/address-validation";
 import { Validation } from "../validation/validation";
@@ -27,6 +29,31 @@ export class AddressService {
     const address = await prismaClient.address.create({
       data: createRequest,
     });
+
+    return toAddressResponse(address);
+  }
+
+  static async get(
+    user: User,
+    request: GetAddressRequest
+  ): Promise<AddressResponse> {
+    const getRequest = Validation.validate(AddressValidation.GET, request);
+
+    await ContactService.checkContactMustExists(
+      user.username,
+      request.contact_id
+    );
+
+    const address = await prismaClient.address.findFirst({
+      where: {
+        id: getRequest.id,
+        contact_id: getRequest.contact_id,
+      },
+    });
+
+    if (!address) {
+      throw new ResponseError(404, "Address is not found");
+    }
 
     return toAddressResponse(address);
   }
